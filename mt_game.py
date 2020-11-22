486c417fd0f3fb3af1a504e2277a6ab8c7cab76f
Jari Kinnunen <jari.kinnunen@opuscapita.com>
2020-11-22 17:20:46 +0200

    Added timer for answering.

--- mt_game.py
+++ mt_game.py
@@ -1,12 +1,30 @@
 """
 This is a multiplication game. If the player gets 10 points the player wins. Correct answer gives one point
-and wrong answer drops points to zero. Printouts are in finnish.
+and wrong answer drops points to zero. There's 10 seconds to answer, exceeding that gives -1 point. Printouts are in finnish.
 
 """
 import random
 import sys
+from inputimeout import inputimeout, TimeoutOccurred
+
+# Function which fetches answer and checks if timeout occurs
+def get_input_with_timeout(prompt, points):
+    no_timeout = False
+    response = ''
+    while not no_timeout:
+        no_timeout = True
+        try:
+            response = inputimeout(prompt=prompt, timeout=10)
+        except TimeoutOccurred:
+            no_timeout = False
+            if points > 0:
+                points -= 1
+            print("Et antanut vastausta ajoissa. Sinulla on", points, "pistettä")
+    return response, points
+
 
 print("Tervetuloa pelaamaan kertolaskupeliä. Saat yhden pisteen oikeasta vastauksesta. Väärästä tiput nollille.")
+print("Aikaa vastaukseen on 10 sekuntia, muuten vähennetään yksi piste.")
 print("Voitat, jos saat 10 pistettä. Kirjoita 'lopeta', jos haluat lopettaa kesken.")
 
 # The multiplication tables to be used in the game are given in this list
@@ -21,7 +39,8 @@ while points < 10:
 
     # The calculation string shown to the user
     calculation = "{} * {} = ".format(multiplier, mt)
-    number = input(calculation)
+    number, points = get_input_with_timeout(calculation, points)
+    #    number = input(calculation)
     correct_answer = mt * multiplier
 
     # While loop is continued until the correct answer is given or the user wants to quit
@@ -33,12 +52,12 @@ while points < 10:
         elif not number.isnumeric():
             # If given character is not a number
             print(number, "ei ole luku", end="\r\n")
-            number = input(calculation)
+            number, points = get_input_with_timeout(calculation, points)
         elif int(number) != correct_answer:
             # If given number is not correct the user is asked to try the same calculation again
             points = 0
             print("Väärin, sinulla on 0 pistettä. Yritä samaa laskua uudelleen", end="\r\n")
-            number = input(calculation)
+            number, points = get_input_with_timeout(calculation, points)
         else:
             # The correct answer breaks the loop
             break
